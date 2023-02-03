b6747b5af58e43753821f24d91dbece20e014626
sheldonhh <xphuangsg@gmail.com>
2023-02-03 11:29:09 +0800

    update

--- automate_process.sh
+++ automate_process.sh
@@ -2,7 +2,7 @@ python batch_run.py /data/0202_patch/ida/speccpu/clang/O0 /data/SSSSSS/SPECCPU20
 python batch_run.py /data/0202_patch/ida/speccpu/clang/O1 /data/SSSSSS/SPECCPU2006/int speccpu_clang_O1
 python batch_run.py /data/0202_patch/ida/speccpu/clang/O2 /data/SSSSSS/SPECCPU2006/int speccpu_clang_O2
 python batch_run.py /data/0202_patch/ida/speccpu/clang/O3 /data/SSSSSS/SPECCPU2006/int speccpu_clang_O3
-python batch_run.py /data/0202_patch/ida/speccpu/clang/O0 /data/SSSSSS/SPECCPU2006/int speccpu_gcc_O0
+python batch_run.py /data/0202_patch/ida/speccpu/gcc/O0 /data/SSSSSS/SPECCPU2006/int speccpu_gcc_O0
 python batch_run.py /data/0202_patch/ida/speccpu/gcc/O1 /data/SSSSSS/SPECCPU2006/int speccpu_gcc_O1
 python batch_run.py /data/0202_patch/ida/speccpu/gcc/O2 /data/SSSSSS/SPECCPU2006/int speccpu_gcc_O2
 python batch_run.py /data/0202_patch/ida/speccpu/gcc/O3 /data/SSSSSS/SPECCPU2006/int speccpu_gcc_O3
